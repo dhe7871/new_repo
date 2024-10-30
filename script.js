39f95c653b980f81f8b8c3c557945c19f144ef73
dhe7871 <prajapatidheeraj25042004@gmail.com>
2024-10-31 02:58:09 +0530

    adding the form to the website

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 var clicks = 0;
+var feedclick = false;
 const search = document.getElementById('search');
 const inputbox = document.getElementById('inputbox');
 const counter = document.getElementById('visiters');
 const semesters = document.getElementById('semesters');
-
+const form = document.getElementById('feedform');
+const feedbtn = document.getElementById('feedbtn')
+const submitbtn = document.getElementById('submitbtn');
 
 semester_links = [
     "https://iitkgpacin-my.sharepoint.com/:f:/g/personal/prajapatidheeraj_kgpian_iitkgp_ac_in/Eia039srhUdOtmT9ZUe2_-4BTHwumb9GPpd0Uphy4wjNvA",
@@ -31,7 +34,7 @@ for (value of semester_links) {
     i++;
 }
 
-const p = fetch('https://api.counterapi.dev/v1/Aeroweb27/counter/up');
+const p = fetch('https://api.counterapi.dev/v1/Aeroweb27test/counter/up');
 p.then((response) => {
     if (!response.ok) {
         console.log('Error in fetching Visiter count...');
@@ -76,4 +79,85 @@ document.addEventListener('keydown', (event) => {
         inputbox.value = '';
         clicks = 1;
     }
+})
+
+feedbtn.addEventListener('click', ()=>{
+    if(!feedclick){
+        form.style.display = 'flex';
+        document.getElementsByClassName('formname')[0].style.justifyContent = 'center';
+        document.getElementById('name').focus()
+        feedbtn.style.borderBottomLeftRadius = '0';
+        feedbtn.style.borderBottomRightRadius = '0';
+        feedbtn.style.backgroundColor = 'whitesmoke';
+        feedbtn.style.color = 'black';
+        // feedbtn.style.fontWeight = '600';
+        feedclick = true;
+    }else{
+
+        form.style.display = 'none';
+        document.getElementsByClassName('formname')[0].style.justifyContent = 'start';
+        feedbtn.style.borderBottomLeftRadius = '20px';
+        feedbtn.style.borderBottomRightRadius = '20px';
+        feedbtn.style.backgroundColor = 'black'
+        feedbtn.style.color = 'whitesmoke'
+        // feedbtn.style.fontWeight = 'normal';
+        feedclick = false;
+    }
+})
+feedbtn.addEventListener('mouseover', ()=>{
+    if(!feedclick){
+        feedbtn.style.backgroundColor = 'whitesmoke';
+        feedbtn.style.color = 'black';
+        feedbtn.style.fontWeight = '600';
+    }
+})
+feedbtn.addEventListener('mouseout', ()=>{
+    if(!feedclick){
+        feedbtn.style.fontWeight = 'normal';
+        feedbtn.style.backgroundColor = 'black';
+        feedbtn.style.color = 'whitesmoke';
+    }
+})
+
+submitbtn.addEventListener('click', ()=>{
+    const formdata = new FormData(form);
+    message = '';
+    for(let [key, value] of formdata.entries()){
+        console.log(key, value)
+        if(!value){
+            if(key == 'name' || key == 'email'){
+                message = `*Please enter your '${key}'.`;
+            }else{
+                message = '*Please give your feedback/suggestions.';
+            }
+            break;
+        }
+        if(key == 'email'){
+            let i;
+            for(i=0; i<value.length;i++){
+                if(value[i] == '@'){
+                    break;
+                }
+            }
+            if(i == value.length){
+                message = '*Please enter a valid email.'
+                break;
+            }
+        }
+    }
+
+    if(!message){
+        document.getElementById('name').value = '';;
+        document.getElementById('email').value = '';
+        document.getElementById('feedback').value = '';
+        document.getElementById('submsnmsg').innerText = 'Feedback/Suggestion submitted successfully.';
+        document.getElementById('submsnmsg').style.color = 'black';
+        document.getElementById('submsnmsg').style.visibility = 'visible';
+    }else{
+        document.getElementById('submsnmsg').innerText = message;
+        document.getElementById('submsnmsg').style.color = 'red';
+        document.getElementById('submsnmsg').style.visibility = 'visible';
+    }
+
+    //code to send data to the cloud is to be written here
 })
